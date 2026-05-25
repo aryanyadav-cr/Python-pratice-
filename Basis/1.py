df26a1cd179f724a3ee8360408b8b5ec2fa59f7b
Aryan Yadav <aryanyadav9660@gmail.com>
2026-05-25 15:39:47 +0530

    Initial commit

---
+++ Basis/1.py
@@ -0,0 +1,108 @@
+# In Python, use # for single-line comments and triple quotes (''' ''') for multi-line comments.
+# name = input("Enter your name: ")
+# print(f"My name is, {name}!")
+# print("This is my first code")
+
+# for i in range(122):
+#     print(i)
+
+#use of type to check datatype
+'''
+name = "Aryan"
+i = 7
+old = False
+a = None
+print(type(old))
+print(type(a))
+print(type(i))
+print(type(name))
+'''
+
+#Print Sum
+'''
+a=1
+b=2
+print(a+b)'''
+
+#Arithmetic Operators
+'''
+a=1
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+'''
+
+#Relational Operators
+'''
+a=1
+b=2
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+'''
+
+#Logical operators
+#And
+# age = 20
+
+# print(age > 18 and age < 25)
+# #Or
+# marks = 35
+
+# print(marks > 40 or marks == 35)
+# #not
+# is_logged_in = True
+
+# print(not is_logged_in)
+
+
+#Type conversion 
+
+# a=2
+# b=2.34
+# sum=a+b
+# print("this is conversion:", sum)
+# print(type(sum))
+
+
+##Type casting
+# a=2
+# b=2.34
+# sum=a+int(b)
+# print("this is type casting:", sum)
+# print(type(sum))
+
+#input pratice 
+# string input
+# a = input("enter your full-name ")
+# print("your full name is",a)
+# print(type(a))
+
+# # integer input
+# a = int(input("enter a number "))
+# print("your full name is",a)
+# print(type(a))
+
+# # float input
+# a = float(input("enter a point number "))
+# print("your full name is",a)
+# print(type(a))
+
+
+#Question of chapter 1 
+
+# WAP to input of 2 number and print their sum.
+
+a=float(input("enter a first number"))
+b=float(input("enter a Second number"))
+
+sum = a+b
+print(sum)
+
---
+++ Basis/tempCodeRunnerFile.py
@@ -0,0 +1,46 @@
+# name = input("Enter your name: ")
+# print(f"My name is, {name}!")
+# print("This is my first code")
+
+# for i in range(122):
+#     print(i)
+
+#use of type to check datatype
+'''
+name = "Aryan"
+i = 7
+old = False
+a = None
+print(type(old))
+print(type(a))
+print(type(i))
+print(type(name))
+'''
+
+#Print Sum
+'''
+a=1
+b=2
+print(a+b)'''
+
+#Arithmetic Operators
+'''
+a=1
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+'''
+
+#Relational Operators
+a=1
+b=2
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
